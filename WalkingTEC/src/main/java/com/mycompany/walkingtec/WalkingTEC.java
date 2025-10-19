15ede46ab5bc3ec340cc51a919c138c50db608ba
ChukiluCR <josedavid.ruizflores80@gmail.com>
2025-10-19 13:26:39 -0600

    Borradores de clases
    Solo son borradores para empezar a programar

---
+++ WalkingTEC/src/main/java/com/mycompany/walkingtec/WalkingTEC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.walkingtec;
+
+/**
+ *
+ * @author josed
+ */
+public class WalkingTEC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
